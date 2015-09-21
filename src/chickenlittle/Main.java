60cb257d2b11e01b044353ccc331da4e6e4a16fe
Eliot Slevin <Eliot.slevin@gmail.com>
2015-09-21 20:11:01 +1200

    Simple renderer

--- src/chickenlittle/Main.java
+++ src/chickenlittle/Main.java
@@ -1,7 +1,35 @@
 package chickenlittle;
 
+import javax.swing.JFrame;
+import renderer.RenderPane;
+
+/**
+ * This method will make a new frame and display a 2d array of chars
+ * @author Eliot
+ *
+ */
+
+
+
 public class Main {
 	public static void main(String[] args) {
-		new UserInterface();
+		JFrame f = new JFrame("Swing Paint Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        RenderPane gameCanvas = new RenderPane();
+        
+        
+       
+        char[][] level =    {{'w','w','e','e'},
+                            {'w','e','p','e'},
+                            {'e','e','e','e'},
+                            {'e','e','e','e'}};
+
+
+        gameCanvas.setLevel(level);
+
+
+        f.add(gameCanvas);
+        f.pack();
+        f.setVisible(true);
 	}
 }
